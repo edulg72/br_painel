2c5616f9fc6b44376f63dc4ea74ceebc3b0de26d
Eduardo Leão Garcia <edulg72@gmail.com>
2018-05-01 13:51:28 -0300

    Apenas sul do Brasil.

--- scripts/buscaPU.sh
+++ scripts/buscaPU.sh
@@ -7,37 +7,36 @@ echo "Inicio: $(date '+%d/%m/%Y %H:%M:%S')"
 psql -h $POSTGRESQL_DB_HOST -d br_painel -U $POSTGRESQL_DB_USERNAME -c 'delete from pu;'
 psql -h $POSTGRESQL_DB_HOST -d br_painel -U $POSTGRESQL_DB_USERNAME -c 'delete from local;'
 
-ruby busca_PU.rb $1 $2 -61.5 6 -59 5 1
-ruby busca_PU.rb $1 $2 -66 5 -59 3 1
-ruby busca_PU.rb $1 $2 -52.5 5 -50 3 1
-ruby busca_PU.rb $1 $2 -69 3 -50 2 1
-ruby busca_PU.rb $1 $2 -67.5 2 -49 0 1
-ruby busca_PU.rb $1 $2 -67.5 0 -43.5 -2 1
-ruby busca_PU.rb $1 $2 -67.5 -2 -37.5 -4 1
-ruby busca_PU.rb $1 $2 -73.5 -4 -34.5 -6 1
-ruby busca_PU.rb $1 $2 -75 -6 -34.5 -8 1
+#ruby busca_PU.rb $1 $2 -61.5 6 -59 5 1
+#ruby busca_PU.rb $1 $2 -66 5 -59 3 1
+#ruby busca_PU.rb $1 $2 -52.5 5 -50 3 1
+#ruby busca_PU.rb $1 $2 -69 3 -50 2 1
+#ruby busca_PU.rb $1 $2 -67.5 2 -49 0 1
+#ruby busca_PU.rb $1 $2 -67.5 0 -43.5 -2 1
+#ruby busca_PU.rb $1 $2 -67.5 -2 -37.5 -4 1
+#ruby busca_PU.rb $1 $2 -73.5 -4 -34.5 -6 1
+#ruby busca_PU.rb $1 $2 -75 -6 -34.5 -8 1
 
-ruby busca_PU.rb $1 $2 -75 -8 -43.5 -9 1
-ruby busca_PU.rb $1 $2 -43.5 -8 -43.2 -8.7 1
+#ruby busca_PU.rb $1 $2 -75 -8 -43.5 -9 1
+#ruby busca_PU.rb $1 $2 -43.5 -8 -43.2 -8.7 1
 # Ghost Town => [-43.5,-8.7,-43.2,-9.0]
-ruby busca_PU.rb $1 $2 -43.2 -8 -34.5 -9 1
+#ruby busca_PU.rb $1 $2 -43.2 -8 -34.5 -9 1
 
-ruby busca_PU.rb $1 $2 -73.5 -9 -34.5 -10 1
+#ruby busca_PU.rb $1 $2 -73.5 -9 -34.5 -10 1
 
 # Com Bahia
-ruby busca_PU.rb $1 $2 -73.5 -10 -34.5 -11 1
-ruby busca_PU.rb $1 $2 -72 -11 -36 -12 1
-ruby busca_PU.rb $1 $2 -66 -12 -36 -14 1
-ruby busca_PU.rb $1 $2 -61.5 -14 -38.5 -17 1
+#ruby busca_PU.rb $1 $2 -73.5 -10 -34.5 -11 1
+#ruby busca_PU.rb $1 $2 -72 -11 -36 -12 1
+#ruby busca_PU.rb $1 $2 -66 -12 -36 -14 1
+#ruby busca_PU.rb $1 $2 -61.5 -14 -38.5 -17 1
 
 # Sem Bahia
-#ruby busca_PU.rb $1 $2 -73.5 -10 -43.7 -11 1
-#ruby busca_PU.rb $1 $2 -38.4 -10 -34.5 -11 1
-#ruby busca_PU.rb $1 $2 -72 -11 -46 -12 1
-#ruby busca_PU.rb $1 $2 -38.2 -11 -36 -12 1
-#ruby busca_PU.rb $1 $2 -66 -12 -46 -14 1
-#ruby busca_PU.rb $1 $2 -61.5 -14 -39.8 -17 1
-#
+ruby busca_PU.rb $1 $2 -73.5 -10 -43.7 -11 1
+ruby busca_PU.rb $1 $2 -38.4 -10 -34.5 -11 1
+ruby busca_PU.rb $1 $2 -72 -11 -46 -12 1
+ruby busca_PU.rb $1 $2 -38.2 -11 -36 -12 1
+ruby busca_PU.rb $1 $2 -66 -12 -46 -14 1
+ruby busca_PU.rb $1 $2 -61.5 -14 -39.8 -17 1
 
 ruby busca_PU.rb $1 $2 -58.5 -17 -38.5 -21 1
 ruby busca_PU.rb $1 $2 -58.5 -21 -40.5 -23 1
